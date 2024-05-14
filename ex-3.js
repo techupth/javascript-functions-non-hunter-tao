049e1edfb30a4cb49cce5c2e7892cfd0d16049b9
Prechak <benedict.precha@gmail.com>
2024-05-14 12:14:08 +0700

    (ex-3): declaring arrow function

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,13 @@
 // Start coding here
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multipy = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multipy(10, 5));
+console.log(divide(10, 5));
