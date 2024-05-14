7e8ac5ab6a7bf14d2bd11c26fc8557fb10f9f6f0
KeeratiNon <keeratinon3@gmail.com>
2024-05-14 12:37:40 +0700

    answer-ex-1-6

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,19 @@
-// Start coding here
+function add (a,b) {
+    return (a+b);
+}
+console.log(add(10,5));
+
+function substract (a,b) {
+    return (a-b);
+}
+console.log(substract(10,5));
+
+function multiply (a,b) {
+    return (a*b);
+}
+console.log(multiply(10,5));
+
+function divide (a,b) {
+    return (a/b);
+}
+console.log(divide(10,5));
