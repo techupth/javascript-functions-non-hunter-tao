23be29adf1da628a0b603bc0ce8734e90e54590b
Prechak <benedict.precha@gmail.com>
2024-05-14 12:06:40 +0700

    (ex-1): declaring the function

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,25 @@
 // Start coding here
+function add(a, b) {
+  let result = a + b;
+  return result;
+}
+
+function subtract(a, b) {
+  let result = a - b;
+  return result;
+}
+
+function multipy(a, b) {
+  let result = a * b;
+  return result;
+}
+
+function divide(a, b) {
+  let result = a / b;
+  return result;
+}
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multipy(10, 5));
+console.log(divide(10, 5));
